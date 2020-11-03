7f33a86c8b842b23389a2984be33f04008803b67
KC Thomas <thisiskcthomas@gmail.com>
2020-11-03 16:41:08 +0000

    Added tests for #land. A plane cannot be landed if the airport is at capacity.

--- jasmine/spec/AirportSpec.js
+++ jasmine/spec/AirportSpec.js
@@ -1,9 +1,29 @@
 describe ('Airport', () => {
   beforeEach(function () {
     airport = new Airport();
-  }) 
+    plane = jasmine.createSpy('plane');
+  }); 
     
-  it('Returns the airport name', ()  => {
-    expect(airport.constructor.name).toEqual('Gatwick');    
+  it('has a default capacity of 10', ()  => {
+    expect(airport.capacity).toEqual(10);    
   })
+
+  it('can change capacity', () => {
+    new_port = new Airport(1500);
+    expect(new_port.capacity).toEqual(1500);
+  })
+
+  describe('#land', () => {
+    it('can land a plane in the airport', () => {
+      airport.land(plane)
+      expect(airport.landed_planes).toContain(plane)
+    })
+
+    it('does not allow planes to land if at capacity', () => {
+      new_port = new Airport(0);
+      expect( function() {new_port.land(plane);} ).toThrow("The bunker is at capacity!");
+    })
+
+  })
+
 })
--- jasmine/src/Airport.js
+++ jasmine/src/Airport.js
@@ -1,5 +1,20 @@
 class Airport {
-  constructor () {
-        
+  constructor (capacity = 10) {
+    this.capacity = capacity;
+    this.landed_planes = []
   };
+
+  land(plane) {
+    console.log(this.capacity);
+    console.log(this.landed_planes)
+    if(this._full()) { 
+      throw "The bunker is at capacity!";
+    }
+    this.landed_planes.push(plane);
+  }
+
+  _full() {
+    return this.capacity === this.landed_planes.length
+  }
+
 }
