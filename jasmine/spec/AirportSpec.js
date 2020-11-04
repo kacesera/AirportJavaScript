896facb2798a0d307714d07fea2591ed5830a30d
KC Thomas <thisiskcthomas@gmail.com>
2020-11-04 16:27:42 +0000

    Added tests, working on bug with 'weather' spy.

--- jasmine/spec/AirportSpec.js
+++ jasmine/spec/AirportSpec.js
@@ -1,8 +1,11 @@
 describe ('Airport', () => {
   beforeEach(function () {
     airport = new Airport();
-    plane = jasmine.createSpyObj('plane', ['is_landed']);
-    plane.is_landed()
+    plane = jasmine.createSpyObj('plane', ['isLanded', 'takeOff']);
+    weather = jasmine.createSpyObj('weather', ['isStormy']);
+    plane.isLanded();
+    plane.takeOff();
+    weather.isStormy();
   }); 
 
     
@@ -30,6 +33,27 @@ describe ('Airport', () => {
       airport.land(plane);
       expect( function() {airport.land(plane);} ).toThrow("This plane has already landed");
     })
+
+    it('does not allow planes to land if the weather is stormy', () => {
+      weather.isStormy.and.returnValue(true);
+      mockPort = new Airport('', weather);
+      expect( function() {mockPort.land(plane);} ).toThrow("It's too stormy to land!");
+    })
+  
+  describe('#takeOff', () => {
+    it("does not allow planes to take off if the weather is stormy", () =>{
+      weather.isStormy.and.returnValues(false, true);
+      mockPort = new Airport('', weather);
+      mockPort.land(plane);
+
+      expect( function() {mockPort.takeOff(plane);} ).toThrow("It's too stormy to fly!");
+    })
+  })
+    // describe "#take_off" do
+    // it "does not allow planes to take off if the weather is stormy" do
+    //   allow(@home_ap.weather).to receive(:stormy?).and_return(true)
+    //   expect { @home_ap.take_off(@plane) }.to raise_error 'It is too stormy to fly'
+    // end
       
       // notes for future reference:
       // plane.is_landed.and.callFake(function() {
--- jasmine/src/Airport.js
+++ jasmine/src/Airport.js
@@ -1,16 +1,26 @@
 class Airport {
-  constructor (capacity = 10) {
+  constructor (capacity = 10, weather = 'weather') {
     this.capacity = capacity;
     this.landed_planes = []
+    this.weather = weather
   };
 
   land(plane) {
+    debugger
     if(this._full()) { 
       throw "The bunker is at capacity!";
     }else if(this._inBunker(plane)) {
       throw "This plane has already landed";
+    }else if(this.weather.isStormy()) {
+      throw "It's too stormy to land!";
     }else {
     this.landed_planes.push(plane);
+    }
+  };
+
+  takeOff(plane) {
+    if(this.weather.isStormy()) {
+      throw "It's too stormy to fly!";
     }
   };
 
